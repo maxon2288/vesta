430fdf1d48eba94f91c34e3925f022be2198fa70
maxswim228 <maxswim228@gmail.com>
2018-09-19 02:01:14 +0600

    fdsafdsafs

--- markup/static/js/plugins/main.js
+++ markup/static/js/plugins/main.js
@@ -5,3 +5,37 @@ $(document).ready(function() {
         $(this).find('img').css('display', 'none');
     });
 });
+
+$(document).ready(function() {
+    $('.new-board-select').niceSelect();
+});
+
+$(".newPopup").click(function () {
+    $(".new-ad-popup, .new-overlay").addClass('visible')
+});
+
+$(".new-ad-popup-close").click(function () {
+    $(".new-ad-popup, .new-overlay").removeClass('visible')
+});
+
+$(function(){  
+    $('.__lk-fileInput input').change(function(){
+      var label = $(this).parent().find('.__lk-fileInput span'); 
+      if(typeof(this.files) !='undefined'){ // fucking IE      
+        if(this.files.length == 0){
+          label.removeClass('withFile').text(label.data('default'));
+        }
+        else{
+          var file = this.files[0]; 
+          var name = file.name;
+          var size = (file.size / 1048576).toFixed(3); //size in mb 
+          label.addClass('.__lk-fileInput span').text(name + ' (' + size + 'mb)');
+        }
+      }
+      else{
+        var name = this.value.split("\\");
+            label.addClass('withFile').text(name[name.length-1]);
+      }
+      return false;
+    });  
+  });
